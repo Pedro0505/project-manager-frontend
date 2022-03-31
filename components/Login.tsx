d0415c20d64bb866112fbc41065236b97ce5dd7e
Pedro Henrique <pedrohrds05@hotmail.com>
2022-03-31 17:40:26 -0300

    feat: login error messages

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,14 +1,26 @@
 import axios, { AxiosResponse } from 'axios';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
+import { MdOutlineWarningAmber } from 'react-icons/md';
 import { storeToken } from '../helpers';
 import styles from '../styles/login.module.css';
 import { ILoginRequest, ILoginResponse } from '../interfaces';
+import errorList from '../helpers/errorList';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [errorExist, setErrorExist] = useState<boolean>(false);
   const router = useRouter();
+
+  const wrongPassword = (error: keyof typeof errorList) => {
+    const message = errorList[error];
+
+    setErrorExist(true);
+
+    setErrorMessage(message || error);
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -29,6 +41,7 @@ function Login() {
       if (axios.isAxiosError(error)) {
         // transfomar os erros em retorno para o usuário
         console.error(error.response);
+        wrongPassword(error.response?.data.error.message);
       }
     }
   };
@@ -55,6 +68,14 @@ function Login() {
           onChange={({ target }) => setPassword(target.value)}
         />
       </label>
+      { errorExist && (
+        <div className={ styles.errorMessage }>
+          <MdOutlineWarningAmber className={ styles.warnigPlate } />
+          <p>
+            { errorMessage }
+          </p>
+        </div>
+      ) }
       <button className={ styles.loginBtn } type="submit">Login</button>
     </form>
   );
---
+++ helpers/errorList.ts
@@ -0,0 +1,10 @@
+const errorList = {
+  'email not found': 'Email não encontrado',
+  '"password" is at least 6 characters long': 'A senha deve conter mais de 6 caracteres',
+  'wrong password': 'A senha está errada',
+  '"email" is not allowed to be empty': 'O email não pode ser vazio',
+  '"password" is not allowed to be empty': 'A senha não pode ser vazia',
+  '"email" must be a valid email': 'O email deve ter o formato jonh@doe.com',
+};
+
+export default errorList;
