89f6045a055e4777a284a69861efb23e3fde48d1
Pedro Henrique <pedrohrds05@hotmail.com>
2022-04-07 11:28:16 -0300

    feat: auth0 validate

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,17 +1,31 @@
 import axios, { AxiosResponse } from 'axios';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdOutlineWarningAmber } from 'react-icons/md';
+import Link from 'next/link';
+import { v4 as uuid } from 'uuid';
+import { useUser } from '@auth0/nextjs-auth0';
 import { storeToken } from '../helpers';
 import styles from '../styles/login.module.css';
-import { ILoginRequest, ILoginResponse } from '../interfaces';
+import { ILoginRequest, ILoginResponse, IRegisterUserRequest, IRegisterUserResponse } from '../interfaces';
 import errorList from '../helpers/errorList';
+
+interface IUser {
+  id: number;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  uuid?: string;
+}
 
 function Login() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [errorMessage, setErrorMessage] = useState<string>('');
   const [errorExist, setErrorExist] = useState<boolean>(false);
+  const [checkUser, setCheckUser] = useState<IUser | boolean>();
+  const { user: auth0User } = useUser();
   const router = useRouter();
 
   const handleError = (error: keyof typeof errorList) => {
@@ -46,6 +60,96 @@ function Login() {
     }
   };
 
+  useEffect(() => {
+    if (auth0User) {
+      const emailExist = async (auth0Email: string) => {
+        try {
+          const endpoint = `${process.env.NEXT_PUBLIC_API_URL}/user/search?q=${auth0Email}`;
+          const { data } = await axios.get<
+          IUser,
+          AxiosResponse<IUser>
+          >(endpoint);
+
+          setCheckUser(data);
+        } catch (error) {
+          if (axios.isAxiosError(error)) {
+            console.log(error);
+            setCheckUser(false);
+          }
+        }
+      };
+
+      const loginAuth0 = async (userEmail: string) => {
+        const loginEndpoint = `${process.env.NEXT_PUBLIC_API_URL}/user/login`;
+
+        const endpoint = `${process.env.NEXT_PUBLIC_API_URL}/user/search?q=${userEmail}`;
+        const { data } = await axios.get<
+        IUser,
+        AxiosResponse<IUser>
+        >(endpoint);
+
+        const user = { email: userEmail, password: data.uuid as string };
+
+        try {
+          const response = await axios.post<
+            ILoginResponse,
+            AxiosResponse<ILoginResponse>,
+            ILoginRequest
+            >(loginEndpoint, user);
+
+          storeToken(response.data.token);
+          router.push('/workspace');
+        } catch (error) {
+          if (axios.isAxiosError(error)) {
+            console.error(error.response);
+          }
+        }
+      };
+
+      const serializeUser = (userEmail: string, name: string) => {
+        const [firstName, lastName] = name.split(' ');
+        const randomPassword = uuid();
+
+        return { firstName,
+          lastName,
+          password: randomPassword,
+          email: userEmail,
+          uuid: randomPassword };
+      };
+
+      const registerAuth0 = async (userEmail: string, name: string) => {
+        const newUser = serializeUser(userEmail, name);
+
+        const endpoint = `${process.env.NEXT_PUBLIC_API_URL}/user/register`;
+
+        try {
+          await axios.post<
+          IRegisterUserResponse,
+          AxiosResponse<IRegisterUserResponse>,
+            IRegisterUserRequest
+          >(endpoint, newUser);
+
+          await loginAuth0(userEmail);
+        } catch (error) {
+          if (axios.isAxiosError(error)) {
+            console.error(error.response);
+          }
+        }
+      };
+
+      const handleAuth0 = async () => {
+        if (auth0User?.email && auth0User?.name) {
+          if (!checkUser) await emailExist(auth0User.email);
+
+          if (checkUser) loginAuth0(auth0User.email);
+          if (checkUser === false) registerAuth0(auth0User.email, auth0User.name);
+        }
+      };
+
+      handleAuth0();
+    }
+  }, [auth0User?.email, router, auth0User, checkUser]);
+
   return (
     <form onSubmit={handleSubmit} className={ styles.formLogin }>
       <label htmlFor="emailLogin">
@@ -77,6 +181,9 @@ function Login() {
         </div>
       ) }
       <button className={ styles.loginBtn } type="submit">Login</button>
+      <div>
+        <Link href="/api/auth/login">Git Hub</Link>
+      </div>
     </form>
   );
 }
