ece5c455ca3a65629d8470ab35802bc440819aac
Matheus Santos <matheusgabriel.18@gmail.com>
2022-04-26 23:56:44 -0300

    test: add cypress initial tests

--- __tests__/integration/first.test.ts
+++ __tests__/integration/first.test.ts
@@ -1,12 +1,52 @@
-describe('primeiro teste', () => {
-  it('alooo', () => {
+describe('E2E tests', () => {
+  before(() => {
     cy.visit('/');
 
-    cy.get('#emailLogin').type('matheus@gmail.com');
-    cy.get('#passwordLogin').type('12345678');
-    cy.contains('Login').click();
+    cy.fixture('login').then(({ matheus }) => {
+      cy.get('#emailLogin').type(matheus.email);
+      cy.get('#passwordLogin').type(matheus.password);
 
-    cy.url().should('include', 'workspace');
+      cy.contains('Login').click();
+
+      cy.saveLocalStorage();
+
+      cy.contains(/store manager/i).click();
+    });
+  });
+
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+  });
+
+  it('Same column card DnD', () => {
+    cy.dragAndDrop(/fazer req 2/i, /fazer req 3/i);
+  });
+
+  it('Card DnD between columns', () => {
+    cy.dragAndDrop(/fazer req 1/i, /fazer req 6/i);
+  });
+
+  it('Columns DnD', () => {
+    cy.dragAndDrop(/fazendo/i, /finalizado/i);
+  });
+
+  it('Create a new card', () => {
+    cy.get('button')
+      .contains('Adicionar card')
+      .then((jQueryButtons) => {
+        const firstButton = jQueryButtons.end()[0];
+
+        cy.wrap(firstButton).click();
+
+        cy.get('textarea').type('New Card');
+        cy.get('button').contains('Confirmar').click();
+
+        cy.get('li').contains('New Card').should('exist');
+      });
   });
 });
 
